3cc0a7ae359a276171fb8f979b60b70b22e99dab
Anasmo1323 <anasmohamed3x2@gmail.com>
2024-11-23 17:07:10 +0200

    backend start

--- backend/app.py
+++ backend/app.py
@@ -6,16 +6,19 @@ import os
 app = Flask(__name__, static_folder="../frontend/dist", static_url_path="/")
 CORS(app)  # Enable CORS for cross-origin requests
 
+
 # API Route
 @app.route('/api/hello', methods=['GET'])
 def hello_world():
     return jsonify(message="Hello from Flask!")
+
 
 # Serve React Frontend
 @app.route('/')
 def serve_react():
     print(f"Serving index.html from {app.static_folder}")
     return send_from_directory(app.static_folder, 'index.html')
+
 
 # Catch-all route to handle React routing
 @app.route('/<path:path>')
@@ -26,6 +29,7 @@ def serve_react_files(path):
     except FileNotFoundError:
         print(f"File {path} not found, serving index.html")
         return send_from_directory(app.static_folder, 'index.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=3000)
