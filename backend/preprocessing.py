451ccc4634ee50ccfb95389c2c6cd7a8ba2ab023
alizayan684 <alizayan684@gmail.com>
2024-12-10 17:43:43 +0200

    Much Optimized Data Selection

--- backend/preprocessing.py
+++ backend/preprocessing.py
@@ -9,29 +9,25 @@ db_url = ApplicationConfig.SQLALCHEMY_DATABASE_URI
 engine = create_engine(db_url)
 
 query_questions = 'SELECT * FROM "Questions"'
-query_signs = 'SELECT * FROM "Signs"'
 
 Questions = pd.read_sql(query_questions, engine)
-Signs = pd.read_sql(query_signs, engine)
 
-imgs = []
-first_question_imgs = Questions['Q_image'][0].split(',')
-for img in first_question_imgs:
-    if img[0] == ' ':
-        img = img[1:]
-    imgs.append(img)
+imgs = Questions['Q_image'][0].split(',')
 
-imgs_info = Signs[Signs['S_translate'].isin(imgs)] 
 
-def display_images(img_paths):
-    num_images = len(img_paths)
+
+def display_images(imgs):
+    num_images = len(imgs)
     cols = 3  
     rows = (num_images // cols) + (num_images % cols > 0)  
 
     fig, axes = plt.subplots(rows, cols, figsize=(15, 5 * rows))
     axes = axes.flatten()  # Flatten the axes array for easy iteration
 
-    for ax, img_path in zip(axes, img_paths):
+    for ax, img in zip(axes, imgs):
+        if img[0] == ' ':
+            img = img[1:]
+        img_path = f'Data/Signs/{img[5:]}.png'
         img = mpimg.imread(img_path)
         ax.imshow(img)
         ax.axis('off')  
@@ -45,4 +41,4 @@ def display_images(img_paths):
 
 
 if __name__ == '__main__':
-    display_images(imgs_info['S_path'])
+    display_images(imgs)
